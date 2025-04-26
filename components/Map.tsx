b4fa4ea6222a66f92e87b6b0fc825b9c9d64eb61
ensured <walnuts@gmail.com>
2025-04-26 12:21:35 -0700

    Add debounce utility from lodash to Map component for improved performance in user interactions.

--- components/Map.tsx
+++ components/Map.tsx
@@ -17,6 +17,7 @@ import {
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { debounce } from 'lodash'
 import {
   Select,
   SelectContent,
